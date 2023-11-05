4ccbd889aa8279a9d2406192905c62338a79afe0
PJ6451 <arcpj6451@gmail.com>
2023-11-05 10:57:39 -0800

    first commit

---
+++ utils.py
@@ -0,0 +1,18 @@
+def get_cols_to_keep():
+    return [
+        "Mine_ID",
+        "MineName",
+        "X",
+        "Y",
+        "Latitude",
+        "Longitude",
+        "ReportYear",
+        "Acres_Dist",
+        "MineStatus",
+        "Operator",
+        "Opt_City",
+        "Owner",
+        "PriProduct",
+        "Opt_Type",
+        "FaceAmount",
+    ]
